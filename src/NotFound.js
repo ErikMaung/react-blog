7dc648d649187079353fa78587e0c32668885b00
Erik Maung <infiniteerik123@gmail.com>
2023-09-04 02:09:02 -0700

    Configure React app for deployment to GitHub Pages

---
+++ src/NotFound.js
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+const NotFound = () => {
+    return (
+        <div className="not-found">
+            <h2>Sorry!</h2>
+            <p>Error 404: That page cannot be found.</p>
+            <div className="links">
+                <Link to="/">Click to Return Home</Link>
+            </div>
+        </div>
+    );
+}
+ 
+export default NotFound;
